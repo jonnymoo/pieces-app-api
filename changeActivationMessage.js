74881344f35d96cfc040300f1e18582e7fcd91c3
jonnymoo <jonnymoo@hotmail.com>
2019-01-06 20:11:20 +0000

    Custom activation email

---
+++ changeActivationMessage.js
@@ -0,0 +1,16 @@
+export async function main(event, context) {
+  // Identify why was this function invoked
+  if (event.triggerSource === "CustomMessage_SignUp") {
+    // Ensure that your message contains event.request.codeParameter. This is the placeholder for code that will be sent
+    const { codeParameter } = event.request;
+    const { userName, region } = event;
+    const { clientId } = event.callerContext;
+    const { email } = event.request.userAttributes;
+    const domain = "piecenotes.com";
+    const link = `<a href="https://${domain}/confirmUser?client_id=${clientId}&user_name=${userName}&confirmation_code=${codeParameter}>Use this link to verify your account</a>`;
+    event.response.emailSubject = "PieceNotes verification link"; // event.request.codeParameter
+    event.response.emailMessage = `Thank you for signing up. ${link}`;
+
+    return success(null);
+  }
+}
