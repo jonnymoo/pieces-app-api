24e6923f2ac11cdec42331f2a77584bfaf83377c
jonnymoo <jonnymoo@hotmail.com>
2019-01-08 05:58:15 +0000

    debug in custom message

--- customMessage.js
+++ customMessage.js
@@ -5,14 +5,16 @@ export function handler(event, context, callback) {
     event.triggerSource === "CustomMessage_ResendCode"
   ) {
     // Ensure that your message contains event.request.codeParameter. This is the placeholder for code that will be sent
+
+    const debug = JSON.stringify(event);
     const { codeParameter } = event.request;
     const { userName, region } = event;
     const { clientId } = event.callerContext;
     const { email } = event.request.userAttributes;
-    const domain = "piecenotes.com";
-    const link = `<a href="https://${domain}/confirmUser?client_id=${clientId}&user_name=${userName}&confirmation_code=${codeParameter}>Use this link to verify your account</a>`;
+    const domain = "api.piecenotes.com/dev";
+    const link = `<a href="https://${domain}/confirmUser?client_id=${clientId}&user_name=${userName}&confirmation_code=${codeParameter}">Use this link to verify your account</a>`;
     event.response.emailSubject = "PieceNotes verification link"; // event.request.codeParameter
-    event.response.emailMessage = `Thank you for signing up. ${link}`;
+    event.response.emailMessage = `Thank you for signing up. ${link}\n${debug}`;
 
     callback(null, event);
   }
